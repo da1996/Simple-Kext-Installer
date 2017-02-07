c71a9862c19cbbfb3ac32e580f3b8424964e6f79
Daniele <daniele@macbook-pro-di-daniele.local>
2017-02-07 22:11:07 +0100

    Initial Commit

---
+++ Kext Installer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kext Installer
+//
+//  Created by Daniele on 07/02/17.
+//  Copyright © 2017 Daniele. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
